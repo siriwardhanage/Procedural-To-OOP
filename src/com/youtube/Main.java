13b427c0e8b010b9fc0a3d5268192fd2414b5133
siriwardhanage <siriwardhanagerashmi@gmail.com>
2023-11-24 22:36:10 +0530

    Reduce the coupling point in Main class *make unused methods private

--- src/com/youtube/Main.java
+++ src/com/youtube/Main.java
@@ -2,14 +2,7 @@ package com.youtube;
 
 public class Main {
     public static void main(String[] args) {
-        Employee employee1 = new Employee();    //create new Employee object
-
-        employee1.setBaseSalary(10);
-
-        employee1.setHourlyRate(20); //assign value "20" to hourlyRate field using getHourlyRate getter
-        int wage = employee1.calculateWage(10); //call calculateWage method in Employee class,
-        // and store the output in int wage variable
-        System.out.println(wage); //print the result
+        Browser browser = new Browser();
     }
 
 
