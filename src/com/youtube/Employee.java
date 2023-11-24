6eb28a15d4b3dc2396b6e85fdbedcb1ccbcf53b6
siriwardhanage <siriwardhanagerashmi@gmail.com>
2023-11-24 17:10:07 +0530

    setter for baseSalary *make baseSalary private *create setBaseSalary() method for validate *In main class call setBaseSalary though Employee class

--- src/com/youtube/Employee.java
+++ src/com/youtube/Employee.java
@@ -5,10 +5,16 @@ package com.youtube;
 
 public class Employee {
 
-    public int baseSalary; //baseSalary field
+    private int baseSalary; //baseSalary field
     public int hourlyRate; //hourlyRate field
 
     public int calculateWage(int extraHours){
         return baseSalary + (hourlyRate * extraHours);
+    }
+
+    public void setBaseSalary(int baseSalary){
+        if (baseSalary <= 0)
+            throw new IllegalArgumentException("Base Salary can be 0 or less");
+        this.baseSalary = baseSalary;
     }
 }
--- src/com/youtube/Main.java
+++ src/com/youtube/Main.java
@@ -3,11 +3,13 @@ package com.youtube;
 public class Main {
     public static void main(String[] args) {
         Employee employee1 = new Employee();    //create new Employee object
-        employee1.baseSalary = 50_000;  //assign value "50_000" to baseSalary field
+        employee1.setBaseSalary(-1);
+
         employee1.hourlyRate = 20; //assign value "20" to hourlyRate field
         int wage = employee1.calculateWage(10); //call calculateWage method in Employee class,
         // and store the output in int wage variable
         System.out.println(wage); //print the result
     }
+
 
 }
